62540c93c8c1b3133a924e4f98b439d14d2a36c7
AL03F13 <aponteeric95@gmail.com>
2015-05-06 14:02:26 -0500

    seis
    se agrego eventos

--- js/acciones.js
+++ js/acciones.js
@@ -3,10 +3,31 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 $('#btnvibrar').on('tap',function(){
 	navigator.vibrate(1000);
-
-}); //tap a btnvibrar
+});
+ //tap a btnvibrar
 $('#btnbeep').on('tap',function(){
-	navigator.beep(1);
+	navigator.notification.beep(1);
+});
+
+$('#izquierda').on('swipeleft', function(){
+	alert("barrio a la Izquierda");
+});
+
+$('#derecha').on('swiperight', function(){
+	alert("barrio a la Derecha");
+});
+
+document.addEventListener("pause",function(){
+	$('#listado').append("<p> se pauso </p>");
+});
+
+document.addEventListener("resume",function(){
+	$('#listado').append("<p>se reinicio</p>");
+})
+//cambiar la orientacion
+$(window).on('orientation change',function(e){
+	$('#listado').append("<p>orientacion: "+ e.orientation +"</p>");
+	
 });
 
 }); 
